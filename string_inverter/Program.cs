5c0b2dba74a1bb1598e8d2c677feeb774de09757
PhilOmar19 <141745067+PhilOmar19@users.noreply.github.com>
2024-07-22 23:06:20 +0800

    First Commit

---
+++ string_inverter/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace string_inverter
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            string text = "opera";
+            string textResult = "";
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                textResult = textResult + text[i];
+            }
+            Console.WriteLine(textResult);
+        }
+    }
+}
